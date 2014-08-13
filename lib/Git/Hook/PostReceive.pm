9883e7f395e06ba89bb1716a8dfe4c4004eff9e4
Yo-An Lin (林佑安) <cornelius@cpan.org>
2014-08-13 02:31:40 +0000

    Import of CORNELIUS/Git-Hook-PostReceive-0.2 from CPAN.
    gitpan-cpan-distribution: Git-Hook-PostReceive
    gitpan-cpan-version:      0.2
    gitpan-cpan-path:         CORNELIUS/Git-Hook-PostReceive-0.2.tar.gz
    gitpan-cpan-author:       CORNELIUS
    gitpan-cpan-maturity:     released

--- lib/Git/Hook/PostReceive.pm
+++ lib/Git/Hook/PostReceive.pm
@@ -1,166 +1,294 @@
 package Git::Hook::PostReceive;
-use v5.12;
-use Moose;
-use methods-invoker;
-use DateTime::Format::DateParse;
+$Git::Hook::PostReceive::VERSION = '0.2';
+use warnings;
+use strict;
+use v5.14;
+use feature "switch";
+
+#ABSTRACT: Parses git commit information in post-receive hook scripts
 use Cwd;
 use File::Basename;
+use Encode;
 
-our $VERSION = '0.01';
-
-method read_stdin ($line) {
-    chomp $line;
-    my @args = split /\s+/, $line;
-    return $->run( @args );
+sub new {
+    my ( $class, %args ) = @_;
+    my $self = bless { utf8 => $args{utf8} ? 1 : 0, }, $class;
+    $self;
 }
 
-method run ($before, $after, $ref) {
-    my $is_new_head = $before =~ /^0{40}/;
-    my $is_delete = $after =~ /^0{40}/;
+sub read_stdin {
+    my $self = shift;
+    my @lines = @_ ? map { split "\n" } @_ : <>;
+    my @branches;
 
-    $before  = $before ne '0000000000000000000000000000000000000000' 
-                ? qx(git rev-parse $before) 
-                : undef;
+    foreach my $line (@lines) {
+        chomp $line;
+        my $payload = $self->run( split /\s+/, $line );
+        if (wantarray) {
+            push @branches, $payload;
+        }
+        else {
+            return $payload;
+        }
+    }
 
-    $after   = $after ne '0000000000000000000000000000000000000000' 
-                ? qx(git rev-parse $after) 
-                : undef;
+    return wantarray ? @branches : ();
+}
 
-    chomp($before) if $before;
-    chomp($after) if $after;
+sub run {
+    my ( $self, $before, $after, $ref ) = @_;
 
-    my ($ref_type,$ref_name) = ( $ref =~ m{refs/([^/]+)/([^/]+)} );
+    return unless $before and $after and $ref;
+
+    #    $before //=
+
+    my ( $created, $deleted ) = ( 0, 0 );
+
+    if ( $before ne '0000000000000000000000000000000000000000' ) {
+        $before = qx(git rev-parse $before);
+        chomp $before;
+    }
+    else {
+        $created = 1;
+    }
+
+    if ( $after ne '0000000000000000000000000000000000000000' ) {
+        $after = qx(git rev-parse $after);
+        chomp $after;
+    }
+    else {
+        $deleted = 1;
+    }
+
     my $repo = getcwd;
-    my @commits = $->get_commits($before,$after);
     return {
         before     => $before,
         after      => $after,
         repository => $repo,
-        ref        => $ref_name,
-        ref_type   => $ref_type,
-        ( $is_new_head 
-            ? (new_head => $is_new_head)
-            : () ),
-        ( $is_delete 
-            ? (delete => $is_delete)
-            : () ),
-        commits    => \@commits,
+        ref        => $ref,
+        created    => $created,
+        deleted    => $deleted,
+        commits    => [ $self->get_commits( $before, $after ) ]
+
+            # head_commit => ... # ?
     };
 }
 
-method get_commits ($before,$after) {
+sub get_commits {
+    my ( $self, $before, $after ) = @_;
+
     my $log_string;
 
-    if( $before && $after ) {
-        $log_string = qx(git rev-list --pretty $before...$after);
+    if (   $before ne '0000000000000000000000000000000000000000'
+        && $after ne '0000000000000000000000000000000000000000' )
+    {
+        $log_string = qx(git rev-list $before...$after);
     }
-    elsif( $after ) {
-        $log_string = qx(git rev-list --pretty $after);
+    elsif ( $after ne '0000000000000000000000000000000000000000' ) {
+        $log_string = qx(git rev-list $after);
     }
 
-    return ( ) unless $log_string;
+    return () unless $log_string;
 
-    my @lines = split /\n/,$log_string;
-    my @commits = ();
-    my $buffer = '';
-    for( @lines ) {
-        if(/^commit\s/ && $buffer ) {
-            push @commits,$buffer;
-            $buffer = '';
+    return reverse map { $self->commit_info($_) } split /\n/, $log_string;
+}
+
+sub commit_info {
+    my ( $self, $hash ) = @_;
+
+    my $commit = qx{git show --format=fuller --date=iso --name-status $hash};
+    $commit = decode( 'utf8', $commit ) if $self->{utf8};
+
+    my @lines = split /\n/, $commit;
+
+    my $info = {
+        added    => [],
+        removed  => [],
+        modified => [] };
+
+    for my $line (@lines) {
+        $_ = $line;
+        if (m{^commit (.*)$}i) {
+            $info->{id} = $1;
         }
-        $buffer .= $_ . "\n";
+        elsif (m{^author:\s+(.*?)\s<(.*?)>}i) {
+            $info->{author} = { name => $1, email => $2 };
+        }
+        elsif (m{^commit:\s+(.*?)\s<(.*?)>}i) {
+            $info->{commiter} = { name => $1, email => $2 };
+        }
+        elsif (m{^authordate:\s+(.*)$}i) {
+            $info->{timestamp} = $1;
+            $info->{timestamp} =~ s/ /T/;
+            $info->{timestamp} =~ s/ ([+-])(\d\d)(\d\d)/$1$2:$3/;
+        }
+        elsif (m{^merge: (\w+)\s+(\w+)}i) {
+            $info->{merge} = { parent1 => $1, parent2 => $2 };
+        }
+        elsif (m{^A\t(.+)}) {
+            push @{ $info->{added} }, $1;
+        }
+        elsif (m{^D\t(.+)}) {
+            push @{ $info->{removed} }, $1;
+        }
+        elsif (m{^M\t(.+)}) {
+            push @{ $info->{modified} }, $1;
+        }
+        elsif (m{^    (.*)}) {
+            $info->{message} .= $1 . "\n";
+        }
     }
-    push @commits, $buffer;
-    return reverse map { 
-                my @lines = split /\n/,$_;
-                my $info = {  };
-                for my $line ( @lines ) {
-                    given($line) {
-                        when( m{^commit (.*)$}i ) { $info->{id} = $1; }
-                        when( m{^author:\s+(.*?)\s<(.*?)>}i ) { 
-                                $info->{author} = { 
-                                    name => $1,
-                                    email => $2
-                                };
-                            }
-                        when( m{^date:\s+(.*)$}i ) {  $info->{date} = DateTime::Format::DateParse->parse_datetime( $1 ); }
-                        when( m{^merge: (\w+)\s+(\w+)} ) { $info->{merge} = { parent1 => $1 , parent2 => $2 } }
-                        default {
-                            $info->{message} .= $line . "\n";
-                        }
-                    }
-                }
-                $info;
-            } @commits;
+    chomp $info->{message};
+    return $info;
 }
 
 1;
+
 __END__
+
+=pod
+
+=encoding UTF-8
+
 =head1 NAME
 
-Git::Hook::PostReceive - 
+Git::Hook::PostReceive - Parses git commit information in post-receive hook scripts
+
+=head1 VERSION
+
+version 0.2
 
 =head1 SYNOPSIS
 
     # hooks/post-receive
     use Git::Hook::PostReceive;
-    my $payload = Git::Hook::PostReceive->new->read_stdin( <STDIN> );
 
-    $payload->{new_head}; 
-    $payload->{delete}; 
+    my @branches = Git::Hook::PostReceive->new->read_stdin;
+    foreach my $payload (@branches) {
 
-    $payload->{before};
-    $payload->{after};
-    $payload->{ref_type}; # tags or heads
+        $payload->{before};
+        $payload->{after};
 
-    for my $commit (@{ $payload->{commits} } ) {
-        $commit->{id};
-        $commit->{author}->{name};
-        $commit->{author}->{email};
-        $commit->{message};
-        $commit->{date};
+        for my $commit (@{ $payload->{commits} } ) {
+            $commit->{id};
+            $commit->{author}->{name};
+            $commit->{author}->{email};
+            $commit->{message};
+            $commit->{date};
+        }
+    }
+
+    # hooks/post-receive to send web hooks like GitHub
+    use Git::Hook::PostReceive 0.2;
+    use LWP::UserAgent;
+    use JSON;
+
+    my $ua = LWP::UserAgent->new;
+    for (Git::Hook::PostReceive->new( utf8 => 1 )->read_stdin) {
+        $ua->post( "http://example.org/webhook", { 'payload' => to_json($_) } );
     }
 
 =head1 DESCRIPTION
 
 Git::Hook::PostReceive parses git commit information in post-receive hook script.
 
-all you need to do is pass the stdin string to Git::Hook::PostReceive, 
-then it returns the commit payload .
+All you need to do is pass each STDIN string to Git::Hook::PostReceive,
+then it returns the commit payload for the particular branch.
 
-=head1 INSTALLATION
+This module does not use any non-core dependencies, so you can also
+copy it to a location of your choice and directly include it.
 
-Git::Hook::PostReceive installation is straightforward. If your CPAN shell is set up,
-you should just be able to do
+To run the hook on an arbitrary git repository, set the C<GIT_WORK_TREE>
+environment variable.
 
-    % cpan Git::Hook::PostReceive
+=head2 payload format
 
-Download it, unpack it, then build it as per the usual:
+The payload format returned by method C<read_stdin> or C<run> is compatible with
+L<https://help.github.com/articles/post-receive-hooks|GitHub Post-Receive Hooks>
+with some minor differences:
 
-    % perl Makefile.PL
-    % make && make test
+    {
+        before  => $commit_hash_before,
+        after   => $commit_hash_after,
+        ref     => $ref,
+        created => $whether_new_branch,      # 1|0 in contrast to true|false
+        deleted => $whether_branch_removed,  # 1|0 in contrast to true|false
+        commits => [
+            id        => $hash,
+            message   => $message,
+            timestamp => $date,
+            author    => {
+                email => $email,
+                name  => $name
+            },
+            commiter  => {
+                email => $email,
+                name  => $name
+            },
+            added     => [@added_paths],
+            removed   => [@deleted_paths],
+            modified  => [@modified_paths],
+        ],
+        repository => $directory,           # in contrast to detailed object
+    }
 
-Then install it:
+C<before> is set to <0000000000000000000000000000000000000000> and C<created>
+is set to C<1> (C<0> otherwise) when a new branch has been pushed. C<after> is
+set to <0000000000000000000000000000000000000000> and C<deleted> is set to C<1>
+(C<0> otherwise) when a branch has been deleted.
 
-    % make install
+=head1 CONFIGURATION
 
-=head1 DOCUMENTATION
+=over 4
 
-Git::Hook::PostReceive documentation is available as in POD. So you can do:
+=item utf8
 
-    % perldoc Git::Hook::PostReceive
+Git does not know about character encodings, so the payload will consists of
+raw byte strings by default.  Setting this configuration value to a true value
+will decode all payload fields as UTF8 to get Unicode strings.
 
-to read the documentation online with your favorite pager.
+=back
+
+=head1 METHODS
+
+=head2 read_stdin( [ @lines ] )
+
+Read one or more lines as passed to a git post-receive hook. One can pass
+arrays of lines or strings that are split by newlines. Lines are read from
+STDIN by default.
+
+=head2 run( $before, $after, $ref )
+
+Return a payload for the commits between C<$before> and C<$after> at branch
+C<$ref>. Returns undef on failure.
+
+=head2 SEE ALSO
+
+L<Git::Repository>, L<Plack::App::GitHub::WebHook>
+
+=head1 CONTRIBUTORS
+
+=over 4
+
+=item *
+
+Jakob Voss <voss@gbv.de>
+
+=item *
+
+Yo-An Lin <cornelius@cpan.org>
+
+=back
 
 =head1 AUTHOR
 
-Yo-An Lin E<lt>cornelius.howl {at} gmail.comE<gt>
+Yo-An Lin
 
-=head1 SEE ALSO
+=head1 COPYRIGHT AND LICENSE
 
-=head1 LICENSE
+This software is copyright (c) 2013 by Yo-An Lin.
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
